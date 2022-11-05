2aff006aa9e93df03a7356b864a81365945123cb
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-05 15:23:51 -0400

    started portfolio.js

---
+++ src/components/portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import portfolio from '../components/portfolio.json'
+import ProjectCards from './projects';
+
+// const titleStyle = {
+//     backgroundColor: '#595959',
+//     padding: '20px',
+//     color: '#ff1a1a'
+// }
