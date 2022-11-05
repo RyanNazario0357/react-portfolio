deed43b0b882e254fb226e0ae9adb88fd7252da1
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-05 14:15:31 -0400

    added resume and headshot

--- src/components/about.js
+++ src/components/about.js
@@ -7,3 +7,21 @@ const imgStyle = {
 const linkStyle = {
     color: 'red'
 }
+
+function About() {
+    return(
+        <div class="jumbotron">
+  <h1 class="display-4">About Me</h1>
+  <img class="headshot" src="https://github.com/RyanNazario0357/react-portfolio/raw/main/public/images/headshot.jpg" alt="headshot" style={imgStyle}></img>
+  <p class="lead">Dedicated Entry level developer. Actively a Keiser University student with an asssociates in MIS and pursuing my bachelors.</p>
+  <hr class="my-4"></hr>
+  <p></p>
+  <p class="lead">
+  <button type="button" class="btn btn-dark"><a href="https://github.com/RyanNazario0357/react-portfolio/raw/main/public/images/Ryan Nazario Resume New(1).pdf
+           " style={linkStyle}>View Full Resume</a></button>
+  </p>
+</div>
+    )
+}
+
+export default About;
