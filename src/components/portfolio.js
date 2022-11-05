c2e9c42c742a4a3911e19e9654951c2388879b94
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-05 15:27:17 -0400

    finished portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -20,3 +20,17 @@ const titleStyle = {
 function Wrapper(props) {
     return <div className="wrapper" style={padding}>{props.children}</div>
 }
+function Portfolio () {
+    return(
+        <section>
+
+            <Wrapper id="project-data">
+                {portfolio.map((project) => (
+                    <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} technologies={project.technologies}/>
+                ))}
+            </Wrapper>
+        </section>
+    )
+}
+
+export default Portfolio;
