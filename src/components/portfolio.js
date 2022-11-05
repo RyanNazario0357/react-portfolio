1dced973b675fd58df039782da20f44653b8be5e
RyanNazario0357 <Ryan0357@icloud.com>
2022-11-05 15:25:48 -0400

    line 11-22

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -8,3 +8,15 @@ import ProjectCards from './projects';
 //     padding: '20px',
 //     color: '#ff1a1a'
 // }
+const padding = {
+    padding: '10px'
+}
+
+const titleStyle = {
+    backgroundColor: 'red',
+    padding: '20px'
+}
+
+function Wrapper(props) {
+    return <div className="wrapper" style={padding}>{props.children}</div>
+}
